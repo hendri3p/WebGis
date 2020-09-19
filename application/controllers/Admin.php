24c21cc33129f102af7f001fe798566dc19d33ad
Titisprawilassari <45716749+Titisprawilassari@users.noreply.github.com>
2020-09-19 13:39:54 +0700

    login

---
+++ application/controllers/Admin.php
@@ -0,0 +1,19 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Admin extends CI_Controller
+{
+
+    public function  index()
+    {
+        $data['title'] = 'Halaman Admin';
+        $data['user'] = $this->db->get_where('user', ['email' =>
+        $this->session->userdata('email')])->row_array();
+
+        $this->load->view('templates/admin_header', $data);
+        $this->load->view('templates/admin_sidebar', $data);
+        $this->load->view('templates/admin_topbar', $data);
+        $this->load->view('admin/index', $data);
+        $this->load->view('templates/admin_footer');
+    }
+}
